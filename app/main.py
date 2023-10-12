cb254fcaf334e63e6c4db70f62901ccb6ebae58c
“Diogo <diogopaiva21@ua.pt>
2023-10-12 21:57:06 +0100

    Done

--- app/main.py
+++ app/main.py
@@ -1,6 +1,42 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+#from pydantic_settings import BaseSettings
+#from fastapi_cognito import CognitoAuth, CognitoSettings, CognitoToken
+#from typing import Any
 
 app = FastAPI()
+
+#class Settings(BaseSettings):
+#    check_expiration: bool = True
+#    jwt_header_prefix: str = "Bearer"
+#    jwt_header_name: str = "Authorization"
+#    userpools: dict[str, dict[str, Any]] = {
+#        "eu": {
+#            "region": "USERPOOL_REGION",
+#            "userpool_id": "USERPOOL_ID",
+#            "app_client_id": "APP_CLIENT_ID"
+#        },
+#        "us": {
+#            "region": "USERPOOL_REGION",
+#            "userpool_id": "USERPOOL_ID",
+#            "app_client_id": "APP_CLIENT_ID"
+#        }
+#    }
+#
+#
+#settings = Settings()
+#
+#cognito_eu = CognitoAuth(
+#    settings=CognitoSettings.from_global_settings(settings)
+#)
+#cognito_us = CognitoAuth(
+#    settings=CognitoSettings.from_global_settings(settings),
+#    userpool_name="us"
+#)
+#
+#
+#@app.get("/")
+#def hello_world(auth: CognitoToken = Depends(cognito_us.auth_required)):
+#    return {"message": "Hello world"}
 
 
 @app.get("/")
