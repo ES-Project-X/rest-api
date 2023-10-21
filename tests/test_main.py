45f718ed39c69781e62aa855c93b4eee1bce796f
Gui <guicostaantunes@gmail.com>
2023-10-21 22:01:13 +0100

    tox setup working

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,4 @@
 from fastapi.testclient import TestClient
-
 from main import app
 
 client = TestClient(app)
@@ -8,7 +7,7 @@ client = TestClient(app)
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"Sup N*ggas"}
+    assert response.json() == {"message":"Sup N*ggas"}
 
 def test_read_register():
     response = client.post("/register", json={"email": "testuser@mail.com", "username": "testuser", "password": "password", "first_name": "test", "last_name": "user", "birth_date": "01/01/2000"})
