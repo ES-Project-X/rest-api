40bcdaa5dc5b2d5042eed847e4b8d08ffbabe19c
GoncaloSilva25 <goncassilva02@gmail.com>
2023-12-11 17:23:28 +0000

    fixed upload i hope

--- crud/file.py
+++ crud/file.py
@@ -1,11 +1,15 @@
+from email.mime import image
+from aiohttp import FormData
 from fastapi.responses import JSONResponse
 from sqlalchemy.orm import Session
-from fastapi import UploadFile
 import boto3
 from dotenv import load_dotenv
 import os
 import urllib.parse
-
+import base64
+from io import BytesIO
+import app.schemas as schemas
+import uuid
 
 load_dotenv(".env")
 BUCKET_NAME = os.environ.get("BUCKET_NAME")
@@ -13,18 +17,28 @@ AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 
 
-async def upload_to_s3(file: UploadFile, db: Session, user_id: int):
+async def upload_to_s3(file: schemas.FileUpload, current_user: str, db: Session):
 
     s3 = boto3.client('s3',aws_access_key_id=AWS_ACCESS_KEY_ID,aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
 
     try:
+        
+        image_type = file.image_type.split("/")[1]
 
-        s3.upload_fileobj(file.file, BUCKET_NAME, file.filename, ExtraArgs={'ACL': 'public-read'})
-        key = file.filename
+        # Generate random UUID for the file name
+        unique_id = uuid.uuid4()
+        # Convert UUID format to hex string
+        file_name = str(unique_id).replace("-", "") + "." + image_type
 
-        url = f'''https://{BUCKET_NAME}.s3.amazonaws.com/{urllib.parse.quote(key, safe="~()*!.'")}'''
+        image_data = base64.b64decode(file.base64_image)
+        image_file = BytesIO(image_data)
 
-        return JSONResponse(status_code=200, content={"message": "File Uploaded Successfully", "url": url})
+        s3.upload_fileobj(image_file, BUCKET_NAME, file_name, ExtraArgs={'ACL': 'public-read'})
+        
+
+        url = f'''https://{BUCKET_NAME}.s3.amazonaws.com/{urllib.parse.quote(file_name, safe="~()*!.'")}'''
+
+        return JSONResponse(status_code=200, content={"message": "File Uploaded Successfully", "image_url": url})
 
     except Exception as e:
         print("Something Happened: ", e)
